8f40ec3e13a89ba9263c16fcf38c2a5b82820506
Olivier Brisse <ouranos@donarproject.org>
2013-02-26 10:41:56 +1100

    Patch the mailer

--- monkey_patches.rb
+++ monkey_patches.rb
@@ -20,3 +20,37 @@ module Redmine
   end
 end
 
+
+# Monkey patch the mailer, to return all issues if no days limit is speficied rather
+# than defaulting to issues due in the next 7 days.
+require 'mailer'
+class Mailer
+  def self.reminders(options={})
+    days = options[:days]
+    project = options[:project] ? Project.find(options[:project]) : nil
+    tracker = options[:tracker] ? Tracker.find(options[:tracker]) : nil
+    user_ids = options[:users]
+
+    scope = Issue.open.where("#{Issue.table_name}.assigned_to_id IS NOT NULL" +
+      " AND #{Project.table_name}.status = #{Project::STATUS_ACTIVE}"
+    )
+    scope = scope.where("#{Issue.table_name}.due_date <= ?", days.day.from_now.to_date) if days.present?
+    scope = scope.where(:assigned_to_id => user_ids) if user_ids.present?
+    scope = scope.where(:project_id => project.id) if project
+    scope = scope.where(:tracker_id => tracker.id) if tracker
+
+    issues_by_assignee = scope.includes(:status, :assigned_to, :project, :tracker).all.group_by(&:assigned_to)
+    issues_by_assignee.keys.each do |assignee|
+      if assignee.is_a?(Group)
+        assignee.users.each do |user|
+          issues_by_assignee[user] ||= []
+          issues_by_assignee[user] += issues_by_assignee[assignee]
+        end
+      end
+    end
+
+    issues_by_assignee.each do |assignee, issues|
+      reminder(assignee, issues, days).deliver if assignee.is_a?(User) && assignee.active?
+    end
+  end
+end
